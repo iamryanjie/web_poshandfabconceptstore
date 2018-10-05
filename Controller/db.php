82712bd691554d390edceff1d564bf7fdf54b11c
EC2 Default User <ec2-user@ip-172-31-4-68.us-east-2.compute.internal>
2018-10-05 17:51:17 +0000

    some changes

--- Controller/db.php
+++ Controller/db.php
@@ -1,27 +1,13 @@
+<?php include "../inc/dbinfo.inc"; ?>
 <?php
-//database connection from web
- // define('DB_HOST', 'bigdata.clgps38zjdow.us-east-2.rds.amazonaws.com:3306');
- // define('DB_USERNAME', 'root');
- // define('DB_PASSWORD', 'abcefg13!#');
 
-// database connection from XAMPP 
-define('DB_HOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'db_poshconceptstorefinal');
+  define('DB_NAME', 'db_poshconceptstorefinal');
 
-// Create connection
-$db = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-	//$db = mysqli_connect("bigdata.clgps38zjdow.us-east-2.rds.amazonaws.com","root",
-		//			"db_poshconceptstorefinal",3306);
-			
-// Check connection
-if (!$db) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+  $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
+  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-
-
-
+//  $database = mysqli_select_db($db, DB_DATABASE);
+  echo "sucess";
 ?>
+
